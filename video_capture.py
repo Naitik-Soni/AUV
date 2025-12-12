73a5848e6450e1099362d9a29b48bf18372abe3b
Naitik Soni <soni.naitik>
2025-12-12 14:50:54 +0530

    Added preprocessing steps and deleted unnecessary files/code

--- video_capture.py
+++ video_capture.py
@@ -1,6 +1,7 @@
 import cv2
+from pre_processing import preprocess, getContouredImage
 
-video_path = r"Input/Vid-2.mp4"
+video_path = r"Input/vid_1.mp4"
 cap = cv2.VideoCapture(video_path)
 
 clahe = cv2.createCLAHE(24, (8,8))
@@ -26,20 +27,31 @@ while True:
     if not ret:
         break
 
-    frame = resize_frame(frame)
+    frame = resize_frame(frame, 0.6)
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    clahe_frame = clahe.apply(gray)
-    lab_image = clahe_on_lab(frame)
+    # gray = cv2.medianBlur(gray, 7)
+    # blurred = cv2.bilateralFilter(gray, 3, 50, 50)
+    # clahe_frame = clahe.apply(gray)
+    # lab_image = clahe_on_lab(frame)
+    # canny = cv2.Canny(blurred, 60, 120)
+    # canny_clahe_frame = cv2.Canny(clahe_frame, 64, 128)
+    preprocessed = preprocess(gray)
+    contours = getContouredImage(frame)
 
     cv2.imshow("Underwater gate", frame)
     cv2.imshow("Underwater gray", gray)
-    cv2.imshow("Underwater clahe", clahe_frame)
-    cv2.imshow("Clahe on LAB", lab_image)
+    cv2.imshow("Preprocessed", preprocessed)
+    cv2.imshow("Contours", contours)
+    # cv2.imshow("Canny", canny)
 
-    cv2.imwrite(fr"Dataset_v1/Original/Gate-1 Frame {frame_index}.png", frame[:490, :])
-    cv2.imwrite(fr"Dataset_v1/Gray/Gate-1 Frame {frame_index}.png", gray[:490, :])
-    cv2.imwrite(fr"Dataset_v1/Contrasted/Gate-1 Frame {frame_index}.png", clahe_frame[:490, :])
-    cv2.imwrite(fr"Dataset_v1/LAB/Gate-1 Frame {frame_index}.png", lab_image[:490, :])
+    # cv2.imshow("Underwater clahe", clahe_frame)
+    # cv2.imshow("Clahe on LAB", lab_image)
+    # cv2.imshow("Canny on CLAHE", canny_clahe_frame)
+
+    # cv2.imwrite(fr"Dataset_v1/Original/Gate-1 Frame {frame_index}.png", frame[:490, :])
+    # cv2.imwrite(fr"Dataset_v1/Gray/Gate-1 Frame {frame_index}.png", gray[:490, :])
+    # cv2.imwrite(fr"Dataset_v1/Contrasted/Gate-1 Frame {frame_index}.png", clahe_frame[:490, :])
+    # cv2.imwrite(fr"Dataset_v1/LAB/Gate-1 Frame {frame_index}.png", lab_image[:490, :])
     
     frame_index += 1
 
